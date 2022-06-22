67deec18e890c51cc34be5a66c4a7cfe56b4d886
anubhaw19 <anubhawapp@gmail.com>
2022-06-22 21:00:17 +0530

    modularize terraform project

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
-
-
 
 # creating a VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
-   tags = {
+  tags = {
     Name = "${var.env_prefix}-vpc"
   }
 
@@ -19,27 +17,31 @@ resource "aws_vpc" "my_vpc" {
    -Route table creation (custom/default)
    -Subnet association to custom route table
     moved to modules/subnet/main.tf */
+module "my-subnet" {
+  source            = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  env_prefix        = var.env_prefix
+  avail_zone        = var.avail_zone
+  vpc_id            = aws_vpc.my_vpc.id
+}
 
-  module "my-subnet" {
-  source = "./modules/subnet" 
-  subnet_cidr_block =  var.subnet_cidr_block
-  env_prefix = var.env_prefix
-  avail_zone = var.avail_zone
-  vpc_id = aws_vpc.my_vpc.id
-  }
 
-  module "my-server" {
-    source              = "./modules/webserver"
-    vpc_id              = aws_vpc.my_vpc.id
-    subnet_id           = module.my-subnet.subnet.id
-    env_prefix          = var.env_prefix
-    avail_zone          = var.avail_zone
-    my_ip               =var.my_ip
-    instance_type       = var.instance_type
-    image_name          = var.image_name
-    public_key_location = var.public_key_location
-    
-  }
+/* -EC2 server
+   -Security group (custom)
+   -SSH key-pair 
+    moved to modules/webserver/main.tf */
+module "my-server" {
+  source              = "./modules/webserver"
+  vpc_id              = aws_vpc.my_vpc.id
+  subnet_id           = module.my-subnet.subnet.id
+  env_prefix          = var.env_prefix
+  avail_zone          = var.avail_zone
+  my_ip               = var.my_ip
+  instance_type       = var.instance_type
+  image_name          = var.image_name
+  public_key_location = var.public_key_location
+
+}
 
 
 
--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,7 +1,7 @@
 #  creating a subnet within VPC
 resource "aws_subnet" "my_subnet-1" {
-  vpc_id     = var.vpc_id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = var.vpc_id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
     Name = "${var.env_prefix}-subnet-1"
@@ -40,7 +40,7 @@ resource "aws_route_table_association" "a-rtb-subnet" {
 # using default (main) route table instead of creating custom -
 
 /*for that we just need to add a internet gateway, no need of associating any subnet,
- because all subnet within a VPC is associate to default (main) route table by Default*/  
+ because all subnet within a VPC is associate to default (main) route table by Default*/
 
 # resource "aws_default_route_table" "main-rtb" {
 #   default_route_table_id = aws_vpc.my_vpc.default_route_table_id
